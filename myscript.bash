f6bc10940a44cbf4dd037b3a8f92395aec5b97b6
Bruno <rafante2@gmail.com>
2019-12-25 21:56:54 -0300

    config for building automatically

---
+++ myscript.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-245.0.0-linux-x86_64.tar.gz
+tar zxvf google-cloud-sdk-245.0.0-linux-x86_64.tar.gz google-cloud-sdk
+./google-cloud-sdk/install.sh
+export PATH="./google-cloud-sdk/bin:$PATH"
+gcloud config set project safiraapi-263018
+# gcloud auth login rafante2@gmail.com --no-launch-browser --force
+./gradlew appengineDeploy
