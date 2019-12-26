ea3d22d176e5760bee1f8fd5ec0068953d4cf4df
Bruno <rafante2@gmail.com>
2019-12-26 00:58:00 -0300

    config for build

--- myscript.bash
+++ myscript.bash
@@ -3,6 +3,7 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 tar zxvf google-cloud-sdk-245.0.0-linux-x86_64.tar.gz google-cloud-sdk
 ./google-cloud-sdk/install.sh
 export PATH="./google-cloud-sdk/bin:$PATH"
+gcloud auth loginrafante2@gmail.com
 gcloud config set project safiraapi-263018
-# gcloud auth login rafante2@gmail.com --no-launch-browser --force
 ./gradlew appengineDeploy
+# gcloud auth login rafante2@gmail.com --no-launch-browser --force
