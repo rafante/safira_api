dba61485f43e8f8ebc24747cb189e1818a073872
Bruno <rafante2@gmail.com>
2019-12-26 02:25:34 -0300

    teste falhou

--- safira-base.bash
+++
@@ -1,4 +0,0 @@
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-245.0.0-linux-x86_64.tar.gz
-tar zxvf google-cloud-sdk-245.0.0-linux-x86_64.tar.gz google-cloud-sdk
-./google-cloud-sdk/install.sh
-export PATH="./google-cloud-sdk/bin:$PATH"
